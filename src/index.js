c06a320280dbb4c084004caf4ae34904a899eb02
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-04-16 15:38:28 -0400

    training changes. make searchbar a class component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
- import React from 'react'
- import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAUNRbvz3pbKA8sBRoL_67mSAA2u_ySxnQ'
 
@@ -7,7 +9,11 @@ const API_KEY = 'AIzaSyAUNRbvz3pbKA8sBRoL_67mSAA2u_ySxnQ'
 
  // below is a component class instantiate by doing <App />
  const App = () => {
-     return <div> Hi! </div>
+     return (
+    <div> 
+         <SearchBar /> 
+    </div>
+    )
  }
 
 
