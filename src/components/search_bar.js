11841a1ac77ddd7c579260dd62d62bd98c8b541c
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-04-17 15:57:26 -0400

    Add constructor for state.term
    make event handler an anon fat arrow func
    
    set input val to state.term val

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { term: ''}
+	}
+
 	render () {
-		return <input onChange={this.onInputChange} />
+		return (
+			<div>
+				
+				<input
+					value = {this.state.term}
+					onChange={(e) => this.setState({ term: e.target.value}) } 
+				/>
+
+			</div>
+		)
 	}
 	
-	onInputChange(event) {
-		console.log(event.target.value)
-	}
 }
 
 export default SearchBar
