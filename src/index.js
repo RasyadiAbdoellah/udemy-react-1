09aaf0cec9e0092b3efef67a9fdbc6fa37b605eb
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-05-08 15:58:47 -0400

    following tutorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAUNRbvz3pbKA8sBRoL_67mSAA2u_ySxnQ'
+
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+    console.log(data)
+})
 
  // Create component, then show on DOM
 
